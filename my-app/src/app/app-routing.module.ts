cb144d527e27fc65c64f8d2665847494dad42542
Scott Lusk <scottlusk.cis@outlook.com>
2023-03-19 21:21:57 -0400

    Start adding msal angular

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserUtils } from '@azure/msal-browser';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+];
+
+const isIframe = window !== window.parent && !window.opener;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    // Don't perform initial navigation in iframes or popups
+   initialNavigation: !BrowserUtils.isInIframe() && !BrowserUtils.isInPopup() ? 'enabledNonBlocking' : 'disabled' // Set to enabledBlocking to use Angular Universal
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -8,6 +8,11 @@ import { MaterialModule } from './material-module';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 
+import { MsalModule } from '@azure/msal-angular';
+import { PublicClientApplication } from '@azure/msal-browser';
+
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +23,18 @@ import { ProfileComponent } from './profile/profile.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    // MsalModule.forRoot( new PublicClientApplication({
+    //   auth: {
+    //     clientId: 'Enter_the_Application_Id_here', // Application (client) ID from the app registration
+    //     authority: 'Enter_the_Cloud_Instance_Id_Here/Enter_the_Tenant_Info_Here', // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
+    //     redirectUri: 'Enter_the_Redirect_Uri_Here'// This is your redirect URI
+    //   },
+    //   cache: {
+    //     cacheLocation: 'localStorage',
+    //     storeAuthStateInCookie: isIE, // Set to true for Internet Explorer 11
+    //   }
+    // }), null, null)
   ],
   providers: [],
   bootstrap: [AppComponent]
