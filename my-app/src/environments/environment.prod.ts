b88e670e75d6b0fc43685b13a84d00866ed0f165
Scott Lusk <scottlusk.cis@outlook.com>
2023-04-09 15:47:33 -0400

    add place holders for auth

--- my-app/src/environments/environment.prod.ts
+++ my-app/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    clientId: 'Enter_the_Application_Id_here', // Application (client) ID from the app registration
+    authority: 'Enter_the_Cloud_Instance_Id_Here/Enter_the_Tenant_Info_Here', // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
+    redirectUri: 'Enter_the_Redirect_Uri_Here'// This is your redirect URI
+  }
 };
--- my-app/src/environments/environment.ts
+++ my-app/src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  auth: {
+    clientId: 'Enter_the_Application_Id_here', // Application (client) ID from the app registration
+    authority: 'Enter_the_Cloud_Instance_Id_Here/Enter_the_Tenant_Info_Here', // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
+    redirectUri: 'Enter_the_Redirect_Uri_Here'// This is your redirect URI
+  }
 };
 
 /*
