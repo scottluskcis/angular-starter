bf69c2fc4ce8ad61b784578ce2017075fbe414f0
Scott Lusk <scottlusk.cis@outlook.com>
2023-04-09 15:37:23 -0400

    update to angular v15 (#3)
    * add msal angular
    
    * Generate home and profile components
    
    * Start adding msal angular
    
    * update to v14
    
    * update to v15
    
    * increase budget

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,16 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { MsalModule } from '@azure/msal-angular';
+import { PublicClientApplication } from '@azure/msal-browser';
+
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    // MsalModule.forRoot( new PublicClientApplication({
+    //   auth: {
+    //     clientId: 'Enter_the_Application_Id_here', // Application (client) ID from the app registration
+    //     authority: 'Enter_the_Cloud_Instance_Id_Here/Enter_the_Tenant_Info_Here', // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
+    //     redirectUri: 'Enter_the_Redirect_Uri_Here'// This is your redirect URI
+    //   },
+    //   cache: {
+    //     cacheLocation: 'localStorage',
+    //     storeAuthStateInCookie: isIE, // Set to true for Internet Explorer 11
+    //   }
+    // }), null, null)
   ],
   providers: [],
   bootstrap: [AppComponent]
